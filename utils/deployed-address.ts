bc61c848922475af2d2869dfabd2f678b1fa8e28
mai01663994599 <mai01663994599@gmail.com>
2024-08-19 07:33:28 +0000

    mai

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x244Db5a8DE54168C35a3DA8591C4a8EBA57Ac597'
+export const ProxyAdmin = '0x0Bc13e43ec8c3e780014175130515561Ff78F793'
+export const SWTRImplementation = '0x872110b353D74d2B5b0DEe1BD6e31304DD58A7fa'
     
